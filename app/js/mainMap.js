f62e216ab380c1bade8b3f88580fa28a906ccc9f
Omar Sánchez Martínez <osanchez@scalapc.com>
2016-06-22 19:49:22 -0500

    Cambio de estilo de mapa y ya no se usa el el mapa road.highway que es el que marcaba todas las autopistas ahora solo se marca el inicio y final de la autopista que importa, se comento el codigo de los mapas

--- app/js/mainMap.js
+++ app/js/mainMap.js
@@ -1,53 +1,21 @@
-var km0 = '<div class="infoWindow"><div class="title"> Inicio de la obra</div><div class="info-image"> <img src="./img/inicioCosoloacaque.jpg" /></div><div class="description"> km 0+000 acayucan cosoleacaque </div></div>'
+var map;
+function initialize() {
 
-var km40Code = '<div class="infoWindow"><div class="title"> Final de la obra </div><div class="info-image"> <img src="./img/finalCosoloacaque.jpg" /></div><div class="description"> Kilometro 40 </div></div>'
+    //vars to show the info window of the main page
+    var km0 = '<div class="infoWindow"><div class="title"> Inicio de la obra</div><div class="info-image"> <img src="./img/inicioCosoloacaque.jpg" /></div><div class="description"> km 0+000 acayucan cosoleacaque </div></div>'
 
-//locations array
-var locations = [
+    var km40Code = '<div class="infoWindow"><div class="title"> Final de la obra </div><div class="info-image"> <img src="./img/finalCosoloacaque.jpg" /></div><div class="description"> Kilometro 40 </div></div>'
+
+    //locations array
+    var locations = [
     [km40Code, 18.008377, -94.589308],
-      [km0, 17.909953, -94.939167]
+    [km0, 17.909953, -94.939167]
     ];
-
-
-//highway map type configrations
-var usRoadMapType = new google.maps.StyledMapType([
-    {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [
-            {
-                hue: '#981d1d'
-            },
-            {
-                saturation: 80
-            },
-            {
-                lightness: -2
-            }
-        ]
-      }, {
-        featureType: 'road.highway',
-        elementType: 'labels',
-        stylers: [
-            {
-                visibility: 'off'
-            },
-            {
-                saturation: 98
-            }
-        ]
-      }
-    ], {
-    name: 'Cosoleacaque'
-});
-
-//main map
-var map = new google.maps.Map(document.getElementById('map_canvas'), {
-    zoom: 11,
+    //Map options
+   var mapOptions = {
+	zoom: 11,
     center: new google.maps.LatLng(18.039871, -94.744801),
-    mapTypeControlOptions: {
-        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'Cosoleacaque']
-    },
+    mapTypeId: google.maps.MapTypeId.TERRAIN,
     scaleControl: false,
     mapTypeControl: false,
     zoomControl: false,
@@ -56,22 +24,67 @@ var map = new google.maps.Map(document.getElementById('map_canvas'), {
     keyboardShortcuts: false,
     maxZoom: 11,
     minZoom: 11
+  };
 
+    //map canvas
+  map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions );
+   //coordinates por the polyline
+      var linePath = [
+    {lat: 18.008377, lng: -94.58930800000002},
+    {lat: 17.98896299121444, lng: -94.60667119994895},
+    {lat: 17.965538646440187, lng: -94.65242716673811},
+    {lat: 17.934264179598323, lng: -94.7036612890704},
+    {lat: 17.926744166338054, lng: -94.7561779319883},
+    {lat: 17.927355391934224, lng: -94.81176028440996},
+    {lat: 17.91351415578552, lng: -94.8720895453713},
+    {lat: 17.909953, lng: -94.939167}
+  ];
 
-});
-//Set map type road.highway
-map.mapTypes.set('Cosoleacaque', usRoadMapType);
-map.setMapTypeId('Cosoleacaque');
+    //Polyline Options
+  var line = new google.maps.Polyline({
+    path: linePath,
+    geodesic: true,
+    strokeColor: '#ff0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 8,
+	editable: false
+  });
 
-//this for show all the markers and content specified on the locations array
+    //this for show all the markers and content specified on the locations array
 for (var i = 0; i < locations.length; i++) {
+    //here is where the marker is set
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map
     });
-
+    //here is where we call the content of the info window
     var infowindow = new google.maps.InfoWindow({
         content: locations[i][0]
     });
     infowindow.open(map, marker);
 }
+     //call to the path coordinates function
+
+  line.addListener('click', function () {
+     prompt('The declaration of the path', getPathVariableCode(line));
+ });
+    //set map
+ line.setMap(map);
+ };
+
+//here we call the initialize function which load the map
+google.maps.event.addDomListener(window, 'load', initialize);
+
+//function to get the coordinates of the polyline
+function getPathVariableCode(line){
+	var codeStr = '  var linePath = [\n';
+	var pathArr = line.getPath();
+	for (var i = 0; i < pathArr.length; i++){
+		codeStr += '    {lat: ' + pathArr.getAt(i).lat() + ', lng: ' + pathArr.getAt(i).lng() + '}' ;
+        if (i !== pathArr.length-1) {
+			codeStr += ',\n';
+		};
+	};
+	codeStr += '\n  ];';
+	return codeStr;
+};
--- app/js/mapPlazasCobro.js
+++ app/js/mapPlazasCobro.js
@@ -1,4 +1,10 @@
-var locations = [
+var map;
+
+function initialize() {
+
+
+    //locations array
+    var locations = [
     ['<div id="fixedWindow">' +
      '<img src="img/PlazasCobro/1.JPG" alt="plaza de cobro">' +
      '<h4 class="fixedWindowTitle">PLAZA DE COBRO 119 ACAYUCAN: TRAMO ISLA: ACAYUCAN - COSOLEACAQUE 0+200.</h4>' +
@@ -13,70 +19,87 @@ var locations = [
 
     ];
 
+    //Map options
+    var mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(17.910078, -94.937305),
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        scaleControl: false,
+        mapTypeControl: false,
+        zoomControl: false,
+        disableDoubleClickZoom: true,
+        keyboardShortcuts: false,
+        minZoom: 11
+    };
 
-var usRoadMapType = new google.maps.StyledMapType([
-    {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [
-            {
-                hue: '#152f60'
-            },
-            {
-                saturation: 80
-            },
-            {
-                lightness: -2
-            }
-        ]
-      }, {
-        featureType: 'road.highway',
-        elementType: 'labels',
-        stylers: [
-            {
-                visibility: 'off'
-            },
-            {
-                saturation: 98
-            }
-        ]
-      }
-    ], {
-    name: 'Cosoleacaque'
-});
+    //map canvas
+    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
-var map = new google.maps.Map(document.getElementById('map_canvas'), {
-    zoom: 13,
-    center: new google.maps.LatLng(17.910078, -94.937305),
-    mapTypeControlOptions: {
-        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'Cosoleacaque']
-    },
-    scaleControl: false,
-    mapTypeControl: false,
-    zoomControl: false,
-    disableDoubleClickZoom: true,
-    keyboardShortcuts: false,
-    minZoom: 11
-});
+    //coordinates por the polyline
+    var linePath = [
+    {lat: 18.008377, lng: -94.58930800000002},
+    {lat: 17.98896299121444, lng: -94.60667119994895},
+    {lat: 17.965538646440187, lng: -94.65242716673811},
+    {lat: 17.934264179598323, lng: -94.7036612890704},
+    {lat: 17.926744166338054, lng: -94.7561779319883},
+    {lat: 17.927355391934224, lng: -94.81176028440996},
+    {lat: 17.91351415578552, lng: -94.8720895453713},
+    {lat: 17.909953, lng: -94.939167}
+  ];
 
-//Set map type road.highway
-map.mapTypes.set('Cosoleacaque', usRoadMapType);
-map.setMapTypeId('Cosoleacaque');
-var infowindow = new google.maps.InfoWindow();
-
-var marker, i;
-
-for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
+    //Polyline Options
+    var line = new google.maps.Polyline({
+        path: linePath,
+        geodesic: true,
+        strokeColor: '#ff0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 8,
+        editable: false
     });
 
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-        return function () {
-            jQuery('#infowindow').css('visibility', 'visible');
-            jQuery('#infowindow').empty();
-            jQuery('#infowindow').append(locations[i][0]);
-        }
-    })(marker, i));
-}
+    //this for show all the markers and content specified on the locations array
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                //With help of jquery we set the info window to the left side
+                jQuery('#infowindow').css('visibility', 'visible');
+                jQuery('#infowindow').empty();
+                jQuery('#infowindow').append(locations[i][0]);
+            }
+        })(marker, i));
+    }
+
+
+    //call to the path coordinates function
+
+    line.addListener('click', function () {
+        prompt('The declaration of the path', getPathVariableCode(line));
+    });
+
+    //set map
+    line.setMap(map);
+};
+
+//here we call the initialize function which load the map
+google.maps.event.addDomListener(window, 'load', initialize);
+
+//function to get the coordinates of the polyline
+function getPathVariableCode(line) {
+    var codeStr = '  var linePath = [\n';
+    var pathArr = line.getPath();
+    for (var i = 0; i < pathArr.length; i++) {
+        codeStr += '    {lat: ' + pathArr.getAt(i).lat() + ', lng: ' + pathArr.getAt(i).lng() + '}';
+        if (i !== pathArr.length - 1) {
+            codeStr += ',\n';
+        };
+    };
+    codeStr += '\n  ];';
+    return codeStr;
+};
